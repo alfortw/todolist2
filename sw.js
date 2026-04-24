6b93cd6eccfc409efaf5bd7263f0fe8dac189085
alfortw <31241516+alfortw@users.noreply.github.com>
2026-04-24 14:15:32 +0900

    最新バージョンでアプリを開く

--- sw.js
+++ sw.js
@@ -1,14 +1,25 @@
 const CACHE_NAME = 'todo-v1';
 const urlsToCache = ['index.html', 'manifest.json'];
 
+
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
-  );
+  // 待機せずにすぐにアクティブ化する
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+self.addEventListener('activate', (event) => {
+  // すべてのクライアント（タブ）を即座に制御下に置く
+  event.waitUntil(self.clients.claim());
+  
+  // 古いキャッシュを削除するロジック（キャッシュ名のバージョンを変える）
+  const CACHE_NAME = 'todo-pro-v2'; // ここを更新する
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    })
   );
 });
